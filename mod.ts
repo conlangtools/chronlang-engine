e8cd6a31b70b94749dd1ed395e35ef4611e87a61
faewd <git@faew.dev>
2024-12-05 20:08:13 +0000

    Add module exports to mod.ts

---
+++ mod.ts
@@ -0,0 +1,5 @@
+export { type ModuleResolver, compileModule } from "./lib/compiler.ts"
+export { Module, type Snapshot } from "./lib/module.ts"
+export type { Language, Tag } from "./lib/diachronics.ts";
+export type { Word } from "./lib/word.ts";
+export type { SoundChange } from "./lib/sound-change.ts"
